f501f1f53ea3acb6edcdedd74c0676057713eb36
Simon Krol <simonkrol11@gmail.com>
2018-01-05 17:55:39 -0500

    clean up json retrieval method

--- validation.py
+++ validation.py
@@ -4,19 +4,16 @@ import math
 url="https://backend-challenge-summer-2018.herokuapp.com/challenges.json"
 cur_list=[]
 def get_json(url, challenge_id):
-	page=1
-	payload={'page':page, 'id':challenge_id}
+	payload={'page':1, 'id':challenge_id}
 	response=requests.get(url, params=payload).json()
 	pagination=response['pagination']
 	total_pages=math.ceil(pagination['total']/pagination['per_page'])
-	page+=1
-	print(total_pages)
-	while(page<=total_pages):
-		payload['page']=page
+	
+	while(payload['page']<total_pages):
+		payload['page']+=1
 		secondary_response=requests.get(url, params=payload).json()
 		for menu in secondary_response['menus']:
 			response['menus'].append(menu)
-		page+=1
 	return response
 
 def check_child(parent_id, child, response):
@@ -30,23 +27,8 @@ def check_child(parent_id, child, response):
 		loop=loop+check_child(child, new_child, response)
 	return loop
 
-# def gen_menu(id, repeat):
-# 	menu_dict=dict()
-# 	total_children=0
-# 	menu_dict['root_id']=id;
-# 	child=set()
-# 	child.update(response['menus'][id-1]['child_ids'])
-# 	total_children+=len(response['menus'][id-1]['child_ids'])
-# 	print(child)
-	
-
-# def get_kids(id):
-# 	return response['menus'][id-1]['child_ids']
-
 response=get_json(url,2)
-# gen_menu(2, False)
-# for menu in response['menus']:
-# 	print(menu)
+
 valid_menus=[]
 invalid_menus=[]
 for menu in response['menus']:
