d433031fe4039be4a841c861d7e05d628f5694f9
Simon Krol <simonkrol11@gmail.com>
2018-01-05 13:10:23 -0500

    compile child node list

--- validation.py
+++ validation.py
@@ -2,6 +2,7 @@ import json
 import requests
 import math
 url="https://backend-challenge-summer-2018.herokuapp.com/challenges.json"
+cur_list=[]
 def get_json(url, challenge_id):
 	page=1
 	payload={'page':page, 'id':challenge_id}
@@ -19,6 +20,7 @@ def get_json(url, challenge_id):
 	return response
 
 def check_child(parent_id, child, response):
+	cur_list.append(child)
 	if(parent_id != response['menus'][child-1].get('parent_id', 0)):
 		return 1
 	if(len(response['menus'][child-1]['child_ids'])==0):
@@ -28,16 +30,33 @@ def check_child(parent_id, child, response):
 		loop=loop+check_child(child, new_child, response)
 	return loop
 
-response=get_json(url,1)
-for menu in response['menus']:
-	print(menu)
+# def gen_menu(id, repeat):
+# 	menu_dict=dict()
+# 	total_children=0
+# 	menu_dict['root_id']=id;
+# 	child=set()
+# 	child.update(response['menus'][id-1]['child_ids'])
+# 	total_children+=len(response['menus'][id-1]['child_ids'])
+# 	print(child)
+	
+
+# def get_kids(id):
+# 	return response['menus'][id-1]['child_ids']
+
+response=get_json(url,2)
+# gen_menu(2, False)
+# for menu in response['menus']:
+# 	print(menu)
+valid_menus=[]
+invalid_menus=[]
 for menu in response['menus']:
 	if('parent_id' not in menu):
 		loop=0
+		cur_list=[]
 		for child in menu['child_ids']:
 			loop=loop+check_child(menu['id'], child, response)
-		print(menu)
-		print(loop)
+		print(cur_list)
+
 
 
 
